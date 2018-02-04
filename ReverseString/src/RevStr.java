8e6b1aecfb4749c079ffeda6b11d225fd8c8e547
Venkat <veerapaneni88@gmail.com>
2018-02-03 20:03:07 -0500

    reverse string project

---
+++ ReverseString/src/RevStr.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class RevStr{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc = new Scanner(System.in);
+String original = sc.nextLine() ;
+System.out.println(original);
+sc.close();
+	}
+
+}
+
+	
+
+
